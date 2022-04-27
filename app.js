9c5e3aae6a659229f6732bba2b894001f3672abc
TOLGA <exbert@exbert.com>
2022-04-27 12:33:59 +0200

    static files added

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
-const fn = function () {
-    console.log("Function working");
-};
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+const port = 3000;
+
+
+// MIDDLEWARE
+app.use(express.static("public"));
+
+
+app.get("/", (req, res) => {
+
+  res.sendFile(path.resolve(__dirname, "temp/index.html"));
+});
+
+app.listen(port, () => {
+  console.log(`Service started on port ${port}`);
+});
